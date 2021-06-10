70568ae9cdbca39f9ac804aa7c9b003548b59437
Jason Hungerford <strydor@gmail.com>
2021-06-10 12:41:47 +0800

    Update Requests.js

--- src/Requests.js
+++ src/Requests.js
@@ -1,6 +1,8 @@
 import React from "react";
+import {getRequests, getRequestsSync} from './Api';
 
 const Requests = () => {
+  console.log('Sync Request Data: ', getRequestsSync())
   return <div>Your code will go here</div>;
 };
 
